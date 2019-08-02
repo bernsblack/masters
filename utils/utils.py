67451cfcf94ca1ba54adbac5bcc992ab2f44ac34
bernard <bernard@zulzi.com>
2019-08-02 14:13:36 +0200

    Added logging configs and minor directory restructuring

--- utils/utils.py
+++ utils/utils.py
@@ -15,6 +15,21 @@ import io
 import base64
 from IPython.display import HTML
 import json
+from datetime import datetime
+
+
+class Timer:
+    def __init__(self):
+        self.cache = datetime.now()
+
+    def check(self):
+        now = datetime.now()
+        duration = now - self.cache
+        self.cache = now
+        return duration.total_seconds()
+
+    def reset(self):
+        self.cache = datetime.now()
 
 
 def read_json(file_name):
