Fix: st_res_net training loop limiter & st_res_net min_max normalisation issue

datasets/grid_dataset.py
@@ -78,8 +78,6 @@ class GridDataGroup:
             # tract_count_grids = zip_file["tract_count_grids"]
             # self.crimes = np.concatenate((self.crimes, tract_count_grids), axis=1)
 
-            self.crimes = self.crimes[:, 0]  # only use totals
-
             # (reminder) ensure that the self.crimes are not scaled to -1,1 before
             self.targets = np.copy(self.crimes[1:])  # only check for totals > 0
 
@@ -111,6 +109,9 @@ class GridDataGroup:
         self.crime_scaler = MinMaxScaler(feature_range=(0, 1))
         self.crime_scaler.fit(self.crimes[self.trn_indices[0]:self.trn_indices[1]], axis=1)
         self.crimes = self.crime_scaler.transform(self.crimes)
+
+        self.crimes = self.crimes[:, 0]  # only select totals after scaling channel wise
+
         trn_crimes = self.crimes[self.trn_indices[0]:self.trn_indices[1]]
         val_crimes = self.crimes[self.val_indices[0]:self.val_indices[1]]
         tst_crimes = self.crimes[self.tst_indices[0]:self.tst_indices[1]]
@@ -118,6 +119,8 @@ class GridDataGroup:
         # targets
         self.targets = np.log2(1 + self.targets)
         self.targets = self.crime_scaler.transform(self.targets)
+        self.targets = self.targets[:, 0]
+
         trn_targets = self.targets[self.trn_indices[0]:self.trn_indices[1]]
         val_targets = self.targets[self.val_indices[0]:self.val_indices[1]]
         tst_targets = self.targets[self.tst_indices[0]:self.tst_indices[1]]

models/model_result.py
@@ -92,8 +92,8 @@ def get_metrics_table(model_metrics):
         data.append(row)
 
     df = pd.DataFrame(columns=col, data=data, index=names)
-    df.sort_values('F1 Score')
     df.index.name = "Model Name"
+    df.sort_values('F1 Score', inplace=True, ascending=False)
 
     return df
 
@@ -138,6 +138,16 @@ class ROCCurve:
 
 class ModelMetrics:  # short memory light way of comparing models - does not save the actually predictions
     def __init__(self, model_name, y_true, y_pred, probas_pred):
+        # y_true must be in format N,1,L to be able to correctly compare all the models
+        if len(y_true.shape) != 3 or y_true.shape[1] != 1:
+            raise Exception(f"y_true must be in (N,1,L) not {y_true.shape}.")
+
+        if len(y_pred.shape) != 3 or y_pred.shape[1] != 1:
+            raise Exception(f"y_pred must be in (N,1,L) not {y_pred.shape}.")
+
+        if len(probas_pred.shape) != 3 or probas_pred.shape[1] != 1:
+            raise Exception(f"probas_pred must be in (N,1,L) not {probas_pred.shape}.")
+
         ap_per_time = average_precision_score_per_time_slot(y_true=y_true,
                                                             probas_pred=probas_pred)
         self.ap_per_time = np.nan_to_num(ap_per_time)
@@ -205,14 +215,14 @@ class ModelResult:
         :param probas_pred (N,1,L): model floating point values, can be likelihoods [0,1) or count estimates [0,n)
         :param t_range (N,1): range of the times of the test set - also used in plots
         """
-        if len(y_true.shape) != 3:
-            raise Exception("y_true must be in (N,1,L) format, try reshaping the data.")
+        if len(y_true.shape) != 3 or y_true.shape[1] != 1:
+            raise Exception(f"y_true must be in (N,1,L) not {y_true.shape}.")
 
-        if len(y_pred.shape) != 3:
-            raise Exception("y_pred must be in (N,1,L) format, try reshaping the data.")
+        if len(y_pred.shape) != 3 or y_pred.shape[1] != 1:
+            raise Exception(f"y_pred must be in (N,1,L) not {y_pred.shape}.")
 
-        if len(probas_pred.shape) != 3:
-            raise Exception("probas_pred must be in (N,1,L) format, try reshaping the data.")
+        if len(probas_pred.shape) != 3 or probas_pred.shape[1] != 1:
+            raise Exception(f"probas_pred must be in (N,1,L) not {probas_pred.shape}.")
 
         self.model_name = model_name
         self.y_true = y_true

models/st_resnet_models.py
@@ -169,6 +169,7 @@ class STResNetOLD(nn.Module):
 
 class STResNet(nn.Module):
     # todo add docs
+    # todo use model_arch instead
 
     def __init__(self,
                  n_layers,
@@ -222,7 +223,7 @@ class STResNet(nn.Module):
         # tanh squeezes values between -1 and 1 that's, that's why the cum-sum wasn't working
         # Last layer is tanh
         if seq_e is None:
-            x_t_hat = torch.tanh(x_res)
+            x_t_hat = torch.sigmoid(x_res)
         else:
             x_ext = self.ext_net(seq_e)
             # todo: ask why are we adding - why not multiply
@@ -318,3 +319,5 @@ class STResNetExtra(nn.Module):
             x_t_hat = torch.sigmoid(x_res + x_ext)  # output values can only be between 0,1 for tanh
 
         return x_t_hat
+
+

utils/metrics.py
@@ -272,16 +272,18 @@ class PRCurvePlotter(BaseMetricPlotter):
         plt.xlim(-0.01, 1.1)
         plt.ylim(0, 1.1)
 
+        beta = 1
         # plot f score contours`
         f_scores = [.4, 0.5, .6, 0.7, .8, .9]
         for i, f_score in enumerate(f_scores):
             x = np.linspace(0.01, 1.1)
-            y = f_score * x / (2 * x - f_score)
+            y = f_score * x / ((1 + (beta ** 2)) * x - f_score * (beta ** 2))
+            # y = f_score * x / (2 * x - f_score)
             if i == 0:  # used to add the f_score label
                 l, = plt.plot(x[y >= 0], y[y >= 0], color='gray', alpha=0.2, label='F1 score')
             else:
                 l, = plt.plot(x[y >= 0], y[y >= 0], color='gray', alpha=0.2)
-            plt.annotate('F1={0:0.1f}'.format(f_score), xy=(0.9, y[45] + 0.02))
+            plt.annotate('F{0}={1:0.1f}'.format(beta,f_score), xy=(0.9, y[45] + 0.02))
 
     def add_curve(self, y_true, probas_pred, label_name):
         precision, recall, thresholds = precision_recall_curve(y_true, probas_pred)